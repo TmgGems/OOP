532e58dadd686ff69428a0e61ec29dfe305391e7
Ghhyamjo Lama <107875745+TmgGems@users.noreply.github.com>
2023-12-08 07:19:49 +0545

    Add files via upload

---
+++ AccessSpcifier/AccessSpcifier/Program1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessSpcifier
+{
+    public class people
+    {
+        public string pName;
+        public int pAge;
+
+        public people()
+        {
+            this.pName = "Gems Tmg";
+            this.pAge = 16;
+        }
+        public void getInfo()
+        {
+            Console.WriteLine("Person Name : {0}",this.pName);
+            Console.WriteLine("Person Age : {0}", this.pAge);
+        }
+    }
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            people p = new people();
+            p.getInfo();
+        }
+    }
+}
---
+++ AccessSpcifier/AccessSpecifier2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccessSpcifier;
+
+class Student : people
+{
+
+}
+
+
+namespace AccessSpecifier2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.getInfo();
+        }
+    }
+}
